1f777381aa68a9aee01a2b5d2ebc4a89d1f11a8c
Abdi Mohamud <abdinajibmohamud@gmail.com>
2021-09-17 20:06:36 -0700

    initialize redux, added state and simple action

--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
-import { StrictMode } from "react";
-import "tailwindcss/tailwind.css"
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./App";
-import { ThemeStore } from "./context";
-import Theme from "./styles/Theme";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import store from "./redux/store";
 import { Provider as ReduxProvider } from "react-redux";
-const rootElement = document.getElementById("root");
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
 ReactDOM.render(
-  <StrictMode>
-    <ReduxProvider store={store}>
-    <Router>
-    <ThemeStore>
-      <Theme>
-    <App />
-    </Theme>
-    </ThemeStore>
-  </Router></ReduxProvider>
-  </StrictMode>
-  ,
-  rootElement
+  <React.StrictMode>
+      <ReduxProvider store={store}>   <App /></ReduxProvider>
+ 
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,9 @@
 import * as actions from "./actions";
 import { combineReducers } from "redux";
 
-const preferences = (state = { theme: false }, action) => {
+const preferences = (state = { theme:{  background: "#272823",
+title: "#6495ed",
+text: "#fff"} }, action) => {
   switch (action.type) {
     case actions.CHANGETHEME:
       return { ...state, theme: action.payload.theme };
